f359a86b15db68485673f8ea6518ae2a8666be40
SnarkosaurusRex <lydiaholtrop@gmail.com>
2018-12-11 23:20:13 -0500

    server methods for categories done

--- server.js
+++ server.js
@@ -13,18 +13,19 @@ app.use('/', express.static(APP_PATH));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Additional middleware which will set headers that we need on each request.
+// Additional middleware which will set headers that we need on each request
 app.use(function(req, res, next) {
     // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
+    // an API server in conjunction with something like webpack-dev-server
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Disable caching so we'll always get the latest comments.
+    // Disable caching so we'll always get the latest
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
 
-//--------------- Route functions start here ---------------\\
+//-------------------- Route functions start here --------------------\\
+// Category-related Routes
 app.get('/api/categories', function(req, res) {
     db.collection('categories').find().toArray(function(err, data) {
         if (err) {
@@ -36,22 +37,81 @@ app.get('/api/categories', function(req, res) {
 });
 
 
-app.post('/api/comments', function(req, res) {
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-        id: Date.now(),
-        author: req.body.author,
-        text: req.body.text,
+app.post('/api/categories', function(req, res) {
+    var newCategory = {
+        catID: Date.now(),
+        name: req.body.name
     };
     
-    db.collection('comments').insertOne(newComment, function(err, result) {
+    db.collection('categories').insertOne(newCategory, function(err, result) {
         if (err) {
             console.error(err);
             process.exit(1);
         }
     });
+    res.send('Category added!'); //should this return just a status code instead?
+});
+
+
+//get all the playlists in the category
+app.get('/api/categories/:id', function (req, res) {
+	var cat = parseInt(req.params['id']);
+
+	db.collection('memberships').find({'catID':cat}, {'plID': 1, _id: 0}).toArray(function(err, data) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		res.json(data);
+	});
+});
+
+
+//edit the category
+app.put('/api/categories/:id', function (req, res) {
+	var cat = parseInt(req.params['id']);
+
+	db.collection('categories').updateOne({'catID':cat}, {$set: {name: req.body.name}}, function(err, result) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+	});
+	res.send('Category updated!'); //return just a status code instead?
+});
+
+
+//delete the category
+app.delete('/api/categories/:id', function (req, res) {
+	var cat = parseInt(req.params['id']);
+
+	db.collection('categories').deleteOne({'catID':cat}, function(err, result) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+	});
+	res.send('Category deleted!'); //return just a status code instead?
+});
+
+
+// Playlist-related Routes
+app.post('/api/playlists', function(req, res) {
+	var newPlaylist = {
+		plID: Date.now(),
+		name: req.body.name,
+		artist: req.body.artist,
+		link: req.body.link
+	};
+
+	db.collection('playlists').insertOne(newPlaylist, function(err, result) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+	});
+
+	//TO-DO: need to figure out how to loop through checkboxes and add the necessary things to the memberships collection
 });
 
 
